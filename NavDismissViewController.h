973943a7570d196056add3ca4cc11a25e59d88f3
Curcio Sobrinho <curcio@me.com>
2014-09-24 08:38:12 -0300

    NavDismissViewController.h

---
+++ NavDismissViewController.h
@@ -0,0 +1,15 @@
+//
+//  NavDismissViewController.h
+//
+//  Created by Curcio J Sobrinho on 24/09/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NavDismissViewController : UINavigationController<UIGestureRecognizerDelegate>
+
+//for ipad
+@property (nonatomic) CGSize mySize;
+
+@end
