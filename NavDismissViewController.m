cc80b98883f42485e4be65c313fc2bf360d487c4
Curcio Sobrinho <curcio@me.com>
2015-07-10 12:49:10 -0300

    Update NavDismissViewController.m
    Added some comments and removed not needed stuff

--- NavDismissViewController.m
+++ NavDismissViewController.m
@@ -46,7 +46,7 @@
         [[UIApplication sharedApplication] setStatusBarHidden:NO];
     }
     
-    //se quiser passar tamanho
+    // if you need to set the size
     
     if (self.mySize.width>0) {
         self.preferredContentSize = CGSizeMake(self.mySize.width,self.mySize.height);
@@ -61,21 +61,11 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
- #pragma mark - Navigation
- 
- // In a storyboard-based application, you will often want to do a little preparation before navigation
- - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
- {
- // Get the new view controller using [segue destinationViewController].
- // Pass the selected object to the new view controller.
- }
- */
-
 -(void) viewDidAppear:(BOOL)animated {
     
     [super viewDidAppear:animated];
     
+    // this is the magic
     if(!tapBehindGesture) {
         tapBehindGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapBehindDetected:)];
         [tapBehindGesture setNumberOfTapsRequired:1];
@@ -84,7 +74,6 @@
     }
     
     UIView *view = [[self.viewControllers lastObject] view];
-    
     [view.window addGestureRecognizer:tapBehindGesture];
     
 }
@@ -93,33 +82,28 @@
     
     [super viewWillDisappear:animated];
     
+    // remove the magic
     UIView *view = [[self.viewControllers lastObject] view];
     [view.window removeGestureRecognizer:tapBehindGesture];
 }
 
 - (void)tapBehindDetected:(UITapGestureRecognizer *)sender
 {
-   // NSLog(@"tapBehindDetected");
-    
+
     if (sender.state == UIGestureRecognizerStateEnded)
     {
         UIView *view = [[self.viewControllers lastObject] view];
         
         CGPoint location = [sender locationInView:nil]; //Passing nil gives us coordinates in the window
-        
-        //NSLog(@"na window=%@ e na view=%@", NSStringFromCGPoint(location), NSStringFromCGPoint([sender locationInView:view]));
-        
+
         if (UIInterfaceOrientationIsLandscape([UIApplication sharedApplication].statusBarOrientation)) {
             location = CGPointMake(location.y, location.x);
         }
-        
-        
-        
+
         //Convert tap location into the local view's coordinate system. If outside, dismiss the view.
         if (![view pointInside:[view convertPoint:location fromView:view.window] withEvent:nil])
         {
             if(view) {
-                
                 [self dismissViewControllerAnimated:YES completion:nil];
             }
         }
@@ -140,15 +124,4 @@
 {
     return YES;
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 @end
